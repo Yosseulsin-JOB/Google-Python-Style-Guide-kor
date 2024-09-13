b9ef2f97d26c171b26572005d2cd9cd2086f3db4
Sotaneum <gnyontu39@gmail.com>
2024-09-13 16:41:54 +0900

    production으로 변경

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
   target: ["web", "es5"],
   module: {
